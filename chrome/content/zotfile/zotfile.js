b785dce7aae95885bce7fb64b4603fbd1515a2e1
Mingwei Zhu <mzhu65536@gmail.com>
2021-09-06 15:30:14 -0400

    add menu option to move entry without renaming

--- chrome/content/zotfile/zotfile.js
+++ chrome/content/zotfile/zotfile.js
@@ -1021,4 +1021,43 @@ Zotero.ZotFile = new function() {
         if(description) progressWin.addDescription(this.ZFgetString('general.warning.skippedAtt.msg'));
         progressWin.startCloseTimer(this.getPref("info_window_duration"));
     });
+    /**
+     * Move select attachments
+     * @return {void}
+     */
+    this.moveSelectedAttachments = Zotero.Promise.coroutine(function* () {
+        // get selected attachments
+        var atts = Zotero.Items.get(this.getSelectedAttachments())
+            .filter(this.checkFileType);
+        // confirm renaming
+        if (this.getPref('confirmation_batch_ask') && atts.length >= this.getPref('confirmation_batch')) 
+            if(!confirm(this.ZFgetString('renaming.moveRename', [atts.length])))
+                return;
+        // show infoWindow        
+        var progressWin = this.progressWindow(this.ZFgetString('renaming.renamed')),
+            description = atts.length == 0;
+        // rename attachments
+        for (let i = 0; i < atts.length; i++) {
+            // get attachment and add line to infoWindow
+            var att = atts[i],
+                progress = new progressWin.ItemProgress(att.getImageSrc(), att.getField('title'));
+            // check attachment
+            if(!(yield att.fileExists()) || att.isTopLevelItem() || this.Tablet.getTabletStatus(att)) {
+                description = true;
+                progress.setError();
+                continue;
+            }
+            // Rename and move attachment
+            att = yield this.renameAttachment(att, this.getPref('import'), false);
+            if(!att) {
+                progress.setError();
+                continue;
+            }
+            // update progress window
+            progress.complete(att.attachmentFilename, att.getImageSrc());
+        }
+        // show messages and handle errors
+        if(description) progressWin.addDescription(this.ZFgetString('general.warning.skippedAtt.msg'));
+        progressWin.startCloseTimer(this.getPref("info_window_duration"));
+    });
 };
